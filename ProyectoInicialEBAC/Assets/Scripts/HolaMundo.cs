c3f0da38489c1536165fa82ad47149bea7b3e800
Luis VR <vaetecweb@gmail.com>
2022-07-04 21:20:09 -0500

    Actualizacion HolaMundo
    -Se modifico el script HolaMundo
    
    -Se usaron distintas funciones para imprimir en la consola de Unity, las cuales son (Start,, Update, Awake, FixedUpdate, LateUpdate).

--- ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -12,10 +12,35 @@ public class HolaMundo : MonoBehaviour
         Debug.LogWarning("Algo salio medianamente mal");
         Debug.LogError("Algo Salio Muy Mal");
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        print("algo paso");
+        Debug.Log("algo paso");
+        Debug.LogWarning("Algo salio medianamente mal");
+        Debug.LogError("Algo Salio Muy Mal");
+    }
+    void Awake ()
+    {
+        print("algo paso");
+        Debug.Log("algo paso");
+        Debug.LogWarning("Algo salio medianamente mal");
+        Debug.LogError("Algo Salio Muy Mal");
+    }
+    void FixedUpdate()
+    {
+     
+       print("algo paso");
+       Debug.Log("algo paso");
+       Debug.LogWarning("Algo salio medianamente mal");
+       Debug.LogError("Algo Salio Muy Mal");
+    }
+    void LateUpdate()
+    {
+
+     print("algo paso");
+     Debug.Log("algo paso");
+     Debug.LogWarning("Algo salio medianamente mal");
+     Debug.LogError("Algo Salio Muy Mal");
     }
 }
